c8e83246169db9b39b810be3ec8a004fa178f786
Oleg Kalyanov <oleg1556@gmail.com>
2018-12-07 17:14:56 +0700

    Add oauth utils in locator and use it

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const OAuth2FlowFactory = require('./lib/OAuth2FlowFactory');
+const OAuth2Utils = require('./lib/OAuth2Utils');
 const ResourceServer = require('./lib/ResourceServer');
 
 module.exports = {
@@ -10,6 +11,9 @@ module.exports = {
 	 * @param {ServiceLocator} locator Catberry's service locator.
 	 */
 	register(locator) {
+		if (!locator.has('oauth2Utils')) {
+			locator.register('oauth2Utils', OAuth2Utils, true);
+		}
 		locator.register('oauth2FlowFactory', OAuth2FlowFactory, true);
 		locator.register('resourceServer', ResourceServer);
 	},
--- test/helpers/environmentHelper.js
+++ test/helpers/environmentHelper.js
@@ -4,6 +4,7 @@ const UHRMock = require('../mocks/UHRMock');
 const express = require('express');
 const events = require('events');
 const OAuth2FlowFactory = require('../../lib/OAuth2FlowFactory');
+const OAuth2Utils = require('../../lib/OAuth2Utils');
 const ServiceLocator = require('catberry-locator');
 
 module.exports = {
@@ -16,6 +17,7 @@ module.exports = {
 		eventBus.on('error', () => {});
 		locator.registerInstance('eventBus', eventBus);
 		locator.register('oauth2FlowFactory', OAuth2FlowFactory, true);
+		locator.register('oauth2Utils', OAuth2Utils, true);
 		locator.register('uhr', UHRMock, true);
 
 		return locator;
